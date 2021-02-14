6b24739b3b918985693e593663429279270ae293
mentorbibaj <mentorbibaj@gmail.com>
2021-02-14 23:34:49 +0100

    Added Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+Vue.component(
+    'articles',
+    require('./components/Articles.vue').default
+);
+Vue.component(
+    'navbar',
+    require('./components/Navbar.vue').default
+);
+
+
+const app = new Vue({
+    el: '#app',
+});
